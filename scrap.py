4789fc760f3b9f80bbc1ef5ef82d8793d0ca668a
Guining Pertin <guining.pertin.iitg@gmail.com>
2018-12-15 19:38:32 +0530

    Continuous run
    Runs continuously now and prints data only when its 'Analysing'

--- scrap.py
+++ scrap.py
@@ -1,19 +1,24 @@
 from bs4 import BeautifulSoup as bs
 import urllib3
+import time
 
 http = urllib3.PoolManager()
 url = '192.168.43.52'
-r = http.request('GET', url)
-html = r.data
 
-soup = bs(html, 'lxml')
-data = soup.find_all('p')
-print(data)
-state = data[0].text[17:]
-N = int(data[1].text[4:])
-P = int(data[2].text[4:])
-K = int(data[3].text[4:])
-print(state)
-print(N)
-print(P)
-print(K)
+while True:
+    r = http.request('GET', url)
+    html = r.data
+    soup = bs(html, 'lxml')
+    data = soup.find_all('p')
+    print(data)
+    state = data[0].text[17:]
+    N = int(data[1].text[4:])
+    P = int(data[2].text[4:])
+    K = int(data[3].text[4:])
+    print(state)
+    if state == 'Analysing':
+        print(N)
+        print(P)
+        print(K)
+    time.sleep(2)
+    
